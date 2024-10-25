591d7395f7f5f406d8e5dd4535d7faf0a957765c
derek <derek2001615@gmail.com>
2024-10-25 15:18:12 +0800

    0

--- cal.py
+++ cal.py
@@ -1,4 +1,4 @@
-def Re_Lp(V_c,L_p,viscosity_o):
+def Re_Lp():
     Re=V_c*L_p/viscosity_o
     return Re
 
@@ -9,7 +9,7 @@ def Re_Lp(V_c,L_p,viscosity_o):
 
     #in exp:Re_Lp 100~600
 
-def colburn_j_factor(L_alpha,L_p,F_p,H,F_d,L_l,T_p,delta_f):
+def colburn_j_factor():
     j=Re_Lp()**-0.487*(L_alpha/90)**0.257*(F_p/L_p)**-0.13*(H/L_p)**-0.29*(F_d/L_p)**-0.235*(L_l/L_p)**0.68*(T_p/L_p)**-0.279*(delta_f/L_p)**-0.05
     return j
 
@@ -22,7 +22,7 @@ def colburn_j_factor(L_alpha,L_p,F_p,H,F_d,L_l,T_p,delta_f):
     #T_p:tube pitch
     #delta_f:Fin thickness
 
-def fanning_friction_factor(L_alpha,L_p,F_p,H,F_d,L_l):
+def fanning_friction_factor():
     f=Re_Lp**-0.781*(L_alpha/90)**0.444*(F_p/L_p)**-1.682*(H/L_p)**-1.22*(F_d/L_p)**-0.818*(L_l/L_p)**1.97
     return f
 
@@ -33,13 +33,13 @@ def fanning_friction_factor(L_alpha,L_p,F_p,H,F_d,L_l):
     #F_d:Flow depth
     #L_l:Louver length
 
-def Nusselt_number(Pr_o):
+def Nusselt_number():
     Nu=colburn_j_factor()*Re_Lp()*Pr_o**(1/3)
     return Nu
 
     #Pr_o:Prandtl number of air
 
-def pressure_drop(A_o,A_c,density_m,V_c):
+def pressure_drop():
     delta_P=fanning_friction_factor()*A_o/A_c*density_m*V_c**2/2
     return delta_P
 
@@ -48,7 +48,7 @@ def pressure_drop(A_o,A_c,density_m,V_c):
     #density_m:Mean average air density
     #V_c:Maximum air velocity 
 
-def h_o(density_m,V_c,Cp_o,Pr_o):
+def h_o():
 
     h=colburn_j_factor()*density_m*V_c*Cp_o/Pr_o**(2/3)
     return h
@@ -59,52 +59,7 @@ def h_o(density_m,V_c,Cp_o,Pr_o):
     #Cp_o:Specific heat of air
     #Pr_o:Prandtl number of air
 
-'''
-def coefficient_m(k_f,delta_f,F_d):
-    m=((2*h_o)/k_f/delta_f*(1+delta_f/F_d))**0.5
-    return m
-
-    #h_o:Heat transfer coefficient
-    #k_f:Thermal conductivity of fin
-    #delta_f:Fin thickness
-    #F_d:Flow depth
-
-def fin_length(H,delta_f):
-    l=H/2+delta_f
-    return l
-
-    #H:Fin height
-    #delta_f:Fin thickness
-
-def eta_f():
-    eta_f=np.tanh(coefficient_m()*fin_length())/coefficient_m()/fin_length()
-    return eta_f
-
-    #eta_f:Fin efficiency
-
-def eta_o(A_f,A_o):
-    eta_o=1-A_f/A_o*(1-eta_f())
-    return eta_o
-    #eta_o:Surface effectiveness
-    #A_f:Fin surface area
-    #A_o:Total air-side surface area
-
-def U_o(A_o,h_i,A_i,delta_w,k_w,A_w):
-    term1=1/eta_o()/h_o()/A_o
-    term2=1/h_i/A_i
-    term3=delta_w/k_w/A_w
-    U_o=1/(term1+term2+term3)/
-    return U_o
-
-    #A_o:Total air-side surface area
-    #h_i:Heat transfer coefficient of water side
-    #A_i:Total water-side surface area
-    #delta_w:Tube wall thickness
-    #k_w:Thermal conductivity of wall
-    #A_w:Tube wall area
-'''
-
-def heat_transfer(A_o,T_o2,T_o1):
+def heat_transfer():
     Q=h_o()*A_o*(T_o2-T_o1)
     return Q
 
@@ -112,6 +67,34 @@ def heat_transfer(A_o,T_o2,T_o1):
     #T_o2:airside oulet temperature
     #T_o1:airside inlet temperature
 
+def set_value():
+    global V_c, L_p, viscosity_o, L_alpha, F_p, H, F_d, L_l, T_p, delta_f, Pr_o, A_o, A_c, density_m, Cp_o, T_o2, T_o1
+    V_c=1               #V_c:Maximum air velocity
+    L_p=1               #L_p:Louver pitch
+    viscosity_o=1       #viscosity_o:Viscosity of air
+    L_alpha=1           #L_alpha:Louver angle
+    F_p=1               #F_p:Fin pitch
+    H=1                 #H:Fin height
+    F_d=1               #F_d:Flow depth
+    L_l=1               #L_l:Louver length
+    T_p=1               #T_p:tube pitch
+    delta_f=1           #delta_f:Fin thickness
+    Pr_o=1              #Pr_o:Prandtl number of air
+    A_o=1               #A_o:actually be A. A:one side of the total surface area of heat exchanger A_o:Total air-side surface area
+    A_c=1               #A_c:Minimum free-flow area for air side
+    density_m=1         #density_m:Mean average air density
+    Cp_o=1              #Cp_o:Specific heat of air
+    T_o2=1              #T_o2:airside oulet temperature
+    T_o1=1              #T_o1:airside inlet temperature
+    return V_c,L_p,viscosity_o,L_alpha,F_p,H,F_d,L_l,T_p,delta_f,Pr_o,A_o,density_m,Cp_o,T_o2,T_o1
 
-
+if __name__ == '__main__':
+    set_value()
+    Re_Lp()
+    colburn_j_factor()
+    fanning_friction_factor()
+    Nusselt_number()
+    pressure_drop()
+    h_o()
+    heat_transfer()
 
